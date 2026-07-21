f33a5e3be2cb977f6e567a6d19ea081bcb526c3d
Aryan Pandit <20252503008@iiitvadodara.ac.in>
2026-07-21 23:35:23 +0530

    vercel insite tool

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,13 @@ import { SubjectProvider } from "./context/SubjectContext";
 import "./Styles/Light.css";
 import "./Styles/Dark.css";
 
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SubjectProvider>
       <App />
+      <SpeedInsights />
     </SubjectProvider>
   </React.StrictMode>
 );
