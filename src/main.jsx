e206fd9aa01929f1d079bc3d5974c535a4dd23d3
Aryan Pandit <20252503008@iiitvadodara.ac.in>
2026-07-21 23:50:23 +0530

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,13 @@ import "./Styles/Light.css";
 import "./Styles/Dark.css";
 
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SubjectProvider>
       <App />
+      <Analytics />
       <SpeedInsights />
     </SubjectProvider>
   </React.StrictMode>
